2b8c10ad82fe4dc18d92f2d3c8a71b5288db5145
Mark Laptop <mark.wheelhouse@imperial.ac.uk>
2019-12-08 18:31:40 +0000

    provided files for the True Concurrency coursework exercise

---
+++ PicStore.c
@@ -0,0 +1,21 @@
+#include "PicStore.h"
+
+void init_picstore(struct pic_store *pstore){
+ //TODO: implement this function
+}    
+
+void print_picstore(struct pic_store *pstore){
+ //TODO: implement this function    
+}
+
+void load_picture(struct pic_store *pstore, const char *path, const char *filename){
+ //TODO: implement this function    
+}
+
+void unload_picture(struct pic_store *pstore, const char *filename){
+ //TODO: implement this function    
+}
+
+void save_picture(struct pic_store *pstore, const char *filename, const char *path){
+ //TODO: implement this function    
+}
